f45ae988bcceba2c97ac63ef74782cf8ace02352
David <dfernandez.alumno@iesrafaelalberti.es>
2023-01-13 03:23:13 +0100

    fixed bug in search state

--- src/components/MoviesGrid.jsx
+++ src/components/MoviesGrid.jsx
@@ -4,12 +4,15 @@ import { useEffect, useState } from 'react'
 import axios from 'axios'
 import { api_key, BASE_URL } from '../api'
 import Spinner from '../components/Spinner'
+import InfiniteScroll from 'react-infinite-scroll-component'
 
 const MoviesGrid = ({ search }) => {
   // Define state for movies
   const [movies, setMovies] = useState([])
   // Define state for loading
   const [loading, setLoading] = useState(true)
+  // Define state for page to fetch next page of movies
+  const [page, setPage] = useState(1)
 
   // Create an instance of axios with the base URL
   const api = axios.create({ baseURL: BASE_URL })
@@ -17,15 +20,14 @@ const MoviesGrid = ({ search }) => {
   // Define a function to get the popular movies from the API using axios and the endpoint /movie/popular
   const getMovies = api.get('/movie/popular', { params: { api_key } })
 
-  const getSearch = api.get('/search/movie', {
-    params: { api_key, query: search },
-  })
-
   // Call the function to get the popular movies and set the state with the response
   useEffect(() => {
     setLoading(true)
     search
-      ? getSearch
+      ? api
+          .get('/search/movie', {
+            params: { api_key, query: search },
+          })
           .then(response => {
             setMovies(response.data.results)
             setLoading(false)
@@ -38,18 +40,24 @@ const MoviesGrid = ({ search }) => {
             setLoading(false)
           })
           .catch(error => console.log(error))
-  }, [search])
+  }, [search, page])
 
   // If the loading state is true, return a loading spinner
   if (loading) return <Spinner />
 
   return (
-    <div className='container-film'>
-      {/* Map over the movies array and render a MovieCard component for each movie */}
-      {movies.map(movie => (
-        <MovieCard key={movie.id} movie={movie} />
-      ))}
-    </div>
+    <InfiniteScroll
+      dataLength={movies.length}
+      hasMore={true}
+      next={() => setPage(prevPage => prevPage + 1)}
+    >
+      <div className='container-film'>
+        {/* Map over the movies array and render a MovieCard component for each movie */}
+        {movies.map(movie => (
+          <MovieCard key={movie.id} movie={movie} />
+        ))}
+      </div>
+    </InfiniteScroll>
   )
 }
 
