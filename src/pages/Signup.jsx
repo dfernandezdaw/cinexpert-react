310637aa88cca3b6df326a2608af1fff0a9b0441
David <dfernandez.alumno@iesrafaelalberti.es>
2023-01-16 10:42:52 +0100

    signup form validation

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link, useNavigate } from 'react-router-dom'
+import { useForm } from 'react-hook-form'
 
 const Signup = () => {
   const [email, setEmail] = React.useState('')
@@ -9,8 +10,13 @@ const Signup = () => {
 
   const navigate = useNavigate()
 
-  const handleSubmit = e => {
-    e.preventDefault()
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm()
+
+  const onSubmit = e => {
     if (password === confpassword && password.length >= 8 && terms === true) {
       localStorage.setItem('email', email)
       localStorage.setItem('password', password)
@@ -32,54 +38,92 @@ const Signup = () => {
       <p style={{ marginBottom: '10px' }}>
         Register and gain access to favourites
       </p>
-      <form id='form' onSubmit={handleSubmit}>
+      <form id='form' onSubmit={handleSubmit(onSubmit)}>
         <label htmlFor='email'>
           <span className='iconify-inline' data-icon='carbon:email'></span>{' '}
           Email Adress
         </label>
         <input
-          type='email'
+          type='text'
+          {...register('email', {
+            required: true,
+            pattern: /^[^\s@]+@[^\s@]+\.[^\s@]+$/i,
+          })}
           name='email'
           id='email'
           placeholder='Email Adress...'
           onChange={e => setEmail(e.target.value)}
-          required
         />
+        {errors.email?.type === 'required' && (
+          <small className='error'>Email is required</small>
+        )}
+        {errors.email?.type === 'pattern' && (
+          <small className='error'>Email is not valid</small>
+        )}
         <label htmlFor='password'>
           <span className='iconify-inline' data-icon='carbon:password'></span>{' '}
           Password
         </label>
         <input
           type='password'
+          {...register('password', {
+            required: true,
+            minLength: 8,
+          })}
           name='password'
           id='password'
           placeholder='Password...'
           onChange={e => setPassword(e.target.value)}
-          required
         />
+        {errors.password?.type === 'required' && (
+          <small className='error'>Password is required</small>
+        )}
+        {errors.password?.type === 'minLength' && (
+          <small className='error'>
+            Password must be at least 8 characters long
+          </small>
+        )}
         <label htmlFor='confpassword'>
           <span className='iconify-inline' data-icon='carbon:password'></span>{' '}
           Confirm password
         </label>
         <input
           type='password'
+          {...register('confpassword', {
+            required: true,
+            minLength: 8,
+          })}
           name='confpassword'
           id='confpassword'
           placeholder='Password...'
           onChange={e => setConfPassword(e.target.value)}
-          required
         />
+        {errors.confpassword?.type === 'required' && (
+          <small className='error'>Password is required</small>
+        )}
+        {errors.confpassword?.type === 'minLength' && (
+          <small className='error'>
+            Password must be at least 8 characters long
+          </small>
+        )}
+        <br />
         <input
           type='checkbox'
+          {...register('terms', {
+            required: true,
+          })}
           name='terms'
           id='terms'
           onChange={e => setTerms(e.target.checked)}
-          required
         />
         <span>
           I agree to<Link to=''>Terms of Service</Link> and
           <Link to=''>Privacy Policy</Link>
         </span>
+        <br />
+        {errors.terms?.type === 'required' && (
+          <small className='error'>You must agree to the terms</small>
+        )}
         <button className='captcha-button' type='submit'>
           Submit
         </button>
